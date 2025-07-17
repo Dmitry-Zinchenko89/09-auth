bcf5a9d9f17401743f19aa48f1ca4c0054e365f7
unknown <zinchenko89d@gmail.com>
2025-07-17 21:33:24 +0300

    fixed API

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
-
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 if (!baseURL) {
     throw new Error('NEXT_PUBLIC_API_URL is not defined in .env.local');
 }
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: `${baseURL}/api`,
     withCredentials: true,
 });
